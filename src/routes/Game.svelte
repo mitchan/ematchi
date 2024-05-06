e42f24ff7625ce0a06c2af772578deb3fd152643
Francesco Casetta <francesco.casetta@gmail.com>
2024-05-06 08:58:59 +0200

    game interface and styles

---
+++ src/routes/Game.svelte
@@ -0,0 +1,29 @@
+<div class="game">
+	<div class="info" />
+
+	<div class="grid-container" />
+
+	<div class="info" />
+</div>
+
+<style>
+	.game {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+	}
+
+	.info {
+		width: 80vmin;
+		height: 10vmin;
+		background-color: rebeccapurple;
+	}
+
+	.grid-container {
+		width: 80vmin;
+		height: 80vmin;
+		background-color: teal;
+	}
+</style>

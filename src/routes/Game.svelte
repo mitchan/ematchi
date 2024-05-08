6287aed950e2e0e3e9667b8e5845403a4ec17a72
Francesco Casetta <francesco.casetta@gmail.com>
2024-05-08 08:16:47 +0200

    feat: game board grid

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -1,7 +1,13 @@
+<script>
+	import Grid from './Grid.svelte';
+</script>
+
 <div class="game">
 	<div class="info" />
 
-	<div class="grid-container" />
+	<div class="grid-container">
+		<Grid />
+	</div>
 
 	<div class="info" />
 </div>
@@ -13,17 +19,18 @@
 		justify-content: center;
 		align-items: center;
 		height: 100%;
+		font-size: min(1vmin, 0.5rem);
 	}
 
 	.info {
-		width: 80vmin;
-		height: 10vmin;
+		width: 80em;
+		height: 10em;
 		background-color: rebeccapurple;
 	}
 
 	.grid-container {
-		width: 80vmin;
-		height: 80vmin;
+		width: 80em;
+		height: 80em;
 		background-color: teal;
 	}
 </style>
---
+++ src/routes/Grid.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Square from './Square.svelte';
+</script>
+
+<div class="grid">
+	{#each Array(16) as _}
+		<Square />
+	{/each}
+</div>
+
+<style>
+	.grid {
+		height: 100%;
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		grid-template-rows: repeat(4, 1fr);
+		gap: 0.5rem;
+	}
+</style>

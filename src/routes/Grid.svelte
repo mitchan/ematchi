2c89aaf178642a09a943ac4e1b335d6ea1da80de
Francesco Casetta <francesco.casetta@gmail.com>
2024-05-08 08:43:13 +0200

    feat: selecting squares

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -2,11 +2,37 @@
 	import Square from './Square.svelte';
 
 	export let grid: string[];
+
+	let a = -1;
+	let b = -1;
+	let reset_timeout: number;
 </script>
 
 <div class="grid">
-	{#each grid as emoji}
-		<Square {emoji} />
+	{#each grid as emoji, i}
+		<Square
+			{emoji}
+			selected={i === a || i === b}
+			on:click={() => {
+				clearTimeout(reset_timeout);
+				if (a === -1 && b === -1) {
+					a = i;
+				} else if (b === -1) {
+					b = i;
+					if (grid[a] === grid[b]) {
+						// correct
+					} else {
+						// wrong
+						reset_timeout = setTimeout(() => {
+							a = b = -1;
+						}, 1000);
+					}
+				} else {
+					a = i;
+					b = -1;
+				}
+			}}
+		/>
 	{/each}
 </div>
 
